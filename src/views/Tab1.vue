9a8bf6f05bc2711f893574593051dc2b1064108b
nahurup <nahuelruppel10@gmail.com>
2021-11-03 13:20:25 -0300

    maqueta de inicio lista

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,27 +2,168 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Inicio</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <!-- Searchbar with value -->
+      <ion-searchbar placeholder="Buscar"></ion-searchbar>
+        <ion-list>
+          <ion-item>
+            <ion-icon slot="end" :icon="trashOutline"></ion-icon>
+            <ion-label>Filtrar:</ion-label>
+          </ion-item>
+          <ion-card-content>
+            <ion-chip color="danger">
+              <ion-icon :icon="heart"></ion-icon>
+              <ion-label>Favoritos</ion-label>
+            </ion-chip>
+            <ion-chip outline color="danger">
+              <ion-icon :icon="heartOutline"></ion-icon>
+              <ion-label>Favoritos</ion-label>
+            </ion-chip>
+            <ion-chip outline color="secondary">
+              <ion-icon :icon="pricetagOutline"></ion-icon>
+              <ion-label>tag1</ion-label>
+            </ion-chip>
+            <ion-chip outline color="tertiary">
+              <ion-icon :icon="pricetagOutline"></ion-icon>
+              <ion-label>tag2</ion-label>
+            </ion-chip>
+            <ion-chip outline color="warning">
+              <ion-icon :icon="pricetagOutline"></ion-icon>
+              <ion-label>tag3</ion-label>
+            </ion-chip>
+            <ion-chip color="secondary">
+              <ion-icon :icon="pricetag"></ion-icon>
+              <ion-label>tag1</ion-label>
+            </ion-chip>
+            <ion-chip color="tertiary">
+              <ion-icon :icon="pricetag"></ion-icon>
+              <ion-label>tag2</ion-label>
+            </ion-chip>
+            <ion-chip color="warning">
+              <ion-icon :icon="pricetag"></ion-icon>
+              <ion-label>tag3</ion-label>
+            </ion-chip>
+          </ion-card-content>
+        </ion-list>
+
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-card @click="presentActionSheet">
+              <img src="https://picsum.photos/200/300" />
+              <ion-card-header>
+                <ion-card-title>Nombre recibo</ion-card-title>
+                <ion-card-subtitle>tag1, tag2, tag3</ion-card-subtitle>
+              </ion-card-header>
+            </ion-card>
+            <ion-card @click="presentActionSheet">
+              <img src="https://picsum.photos/200/300" />
+              <ion-card-header>
+                <ion-card-title>Nombre recibo</ion-card-title>
+                <ion-card-subtitle>tag1, tag2, tag3</ion-card-subtitle>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <ion-card @click="presentActionSheet">
+              <img src="https://picsum.photos/200/300" />
+              <ion-card-header>
+                <ion-card-title>Nombre recibo</ion-card-title>
+                <ion-card-subtitle>tag1, tag2, tag3</ion-card-subtitle>
+              </ion-card-header>
+            </ion-card>
+            <ion-card @click="presentActionSheet">
+              <img src="https://picsum.photos/200/300" />
+              <ion-card-header>
+                <ion-card-title>Nombre recibo</ion-card-title>
+                <ion-card-subtitle>tag1, tag2, tag3</ion-card-subtitle>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonSearchbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonLabel } from '@ionic/vue';
+import { actionSheetController } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { folderOpen, trashOutline, caretForwardCircle, close, heart, trash, share, checkmarkCircle, pricetag, heartOutline, pricetagOutline } from 'ionicons/icons';
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+
+export default defineComponent({
+  components: { IonSearchbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonLabel },
+  setup() {
+    return { checkmarkCircle,
+      heart,
+      heartOutline,
+      pricetagOutline,
+      pricetag,
+      trashOutline,
+      folderOpen }
+  },
+  methods: {
+    async presentActionSheet() {
+      const actionSheet = await actionSheetController
+        .create({
+          header: 'Nombre recibo',
+          cssClass: 'my-custom-class',
+          buttons: [
+            {
+              text: 'Borrar',
+              role: 'destructive',
+              icon: trash,
+              handler: () => {
+                console.log('Delete clicked')
+              },
+            },
+            {
+              text: 'Compartir',
+              icon: share,
+              handler: () => {
+                console.log('Share clicked')
+              },
+            },
+            {
+              text: 'Abrir',
+              icon: folderOpen,
+              handler: () => {
+                console.log('Play clicked')
+              },
+            },
+            {
+              text: 'Favorito',
+              icon: heart,
+              handler: () => {
+                console.log('Favorite clicked')
+              },
+            },
+            {
+              text: 'Cancelar',
+              icon: close,
+              role: 'cancel',
+              handler: () => {
+                console.log('Cancel clicked')
+              },
+            },
+          ],
+        });
+      await actionSheet.present();
+
+      const { role } = await actionSheet.onDidDismiss();
+      console.log('onDidDismiss resolved with role', role);
+    },
+  },
+});
 </script>
+
+<style scoped>
+  ion-card {
+    margin: 0 !important;
+  }
+</style>
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -4,8 +4,8 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="home" />
+          <ion-label>Inicio</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
@@ -24,7 +24,7 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, home } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
@@ -33,7 +33,7 @@ export default {
     return {
       ellipse, 
       square, 
-      triangle,
+      home,
     }
   }
 }
